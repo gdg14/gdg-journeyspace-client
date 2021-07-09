de763d553a0997d604426a6f10a88fe91ca381f9
DESKTOP-LMDPJ93\Administrator <ygm12345@naver.com>
2021-07-09 23:27:33 +0900

    feat: 홈 페이지 라우터에 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,15 @@
 import { Switch, Route, useLocation } from 'react-router-dom';
 import MainCard from '../components/template/MainCard';
 
+import HomeMain from '../components/home/HomeMain';
+
 function Home() {
   const { pathname } = useLocation();
 
   return (
     <MainCard>
       <Switch>
-        <Route path={`${pathname}/`} exact component={() => <div>홈</div>} />
+        <Route path={`${pathname}/`} exact component={HomeMain} />
         <Route path={`${pathname}/journey`} exact component={() => <div>일기쓰기</div>} />
         <Route path={`${pathname}/random`} exact component={() => <div>둘러보기</div>} />
         <Route path={`${pathname}/favorites`} exact component={() => <div>즐겨찾기</div>} />
