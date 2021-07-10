017702d7a74167f5f72a0f517c9d63a29365573b
DESKTOP-LMDPJ93\Administrator <ygm12345@naver.com>
2021-07-11 04:30:59 +0900

    feat: 다이어리 작성된 날 스타일 적용

--- src/components/home/calender/Week.tsx
+++ src/components/home/calender/Week.tsx
@@ -2,7 +2,7 @@ import moment from 'moment';
 import styled from 'styled-components';
 import { useReactiveVar } from '@apollo/client';
 
-import calenderDate from '../../../stores/Calender';
+import calenderDate, { nowMonthDiaries } from '../../../stores/Calender';
 import { WeekWrapper, DayText } from '../../common/Calender';
 
 const DayWrapper = styled.div`
@@ -21,17 +21,31 @@ const TodayCircle = styled.div`
   left: -4px;
 `;
 
+const DayPlanet = styled.img`
+  position: absolute;
+  top: -25px;
+  left: -14px;
+
+  width: 70px;
+  height: 70px;
+
+  opacity: 0.4;
+  cursor: pointer;
+`;
+
 interface IProps {
   firstDayOfThisWeek: moment.Moment;
 }
 
 function Week({ firstDayOfThisWeek }: IProps) {
   const date = useReactiveVar(calenderDate);
+  const diaries = useReactiveVar(nowMonthDiaries);
 
   return (
     <WeekWrapper>
       {Array.from(Array(7).keys()).map((item) => {
         const day = moment(firstDayOfThisWeek).add(item, 'd');
+        const diary = diaries.filter((item) => item.regDtm.isSame(day, 'day'));
 
         return (
           <DayWrapper>
@@ -39,6 +53,12 @@ function Week({ firstDayOfThisWeek }: IProps) {
               {day.format('D')}
             </DayText>
             {day.isSame(moment(), 'day') && <TodayCircle />}
+            {diary.length > 0 && (
+              <DayPlanet
+                alt={`diary_planet_${item}`}
+                src={`${process.env.PUBLIC_URL}/planet/selected/${diary[0].feelings}.png`}
+              />
+            )}
           </DayWrapper>
         );
       })}
