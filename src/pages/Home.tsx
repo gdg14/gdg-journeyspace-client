30dbf6c306602fa64d20e474731bd2c18eec5fc3
DESKTOP-LMDPJ93\Administrator <ygm12345@naver.com>
2021-07-10 21:50:48 +0900

    chore: 코드 띄어쓰기 수정

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,17 @@
-import { Switch, Route, useLocation } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import MainCard from '../components/template/MainCard';
 
 import HomeMain from '../components/home/HomeMain';
 import Journey from '../components/journey/Journey';
+import Random from '../components/random/Random';
 
 function Home() {
-  const { pathname } = useLocation();
-  console.log(pathname)
   return (
     <MainCard>
-        <Route path="/home" component={HomeMain} />
-        <Route path="/journey" component={Journey} />
-        <Route path="/random" exact component={() => <div>둘러보기</div>}  />
-        <Route path="/favorites" exact component={() => <div>즐겨찾기</div>}  />
+      <Route path="/home" component={HomeMain} />
+      <Route path="/journey" component={Journey} />
+      <Route path="/random" exact component={Random} />
+      <Route path="/favorites" exact component={() => <div>즐겨찾기</div>} />
     </MainCard>
   );
 }
