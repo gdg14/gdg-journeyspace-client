c02f3449436d4a70bc4fce6a1d4146836a7e07ad
DESKTOP-LMDPJ93\Administrator <ygm12345@naver.com>
2021-07-10 13:33:03 +0900

    feat: 기본 font family 적용

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -2,12 +2,19 @@ import { createGlobalStyle } from 'styled-components';
 import reset from 'styled-reset';
 
 const GlobalStyle = createGlobalStyle`
+    @font-face {
+        font-family: 'NanumSquare';
+        src: local('./fonts/NanumSquare_acB.ttf');
+    }
+
     ${reset}
 
     * {
         box-sizing: border-box;
 
         margin: 0;
+
+        font-family: NanumSquare !important;
     }
 
     html, body, #root {
